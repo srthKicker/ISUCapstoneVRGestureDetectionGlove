79d38f68aaa1eba96dae92ad30ac8eb9a81d12b4
srthKiker <mesckewmcevs@yahoo.com>
2026-01-18 18:08:49 -0700

    Saving work before reverting

--- FirmwareV0.11/main/bhi360_i2c.h
+++ FirmwareV0.11/main/bhi360_i2c.h
@@ -6,8 +6,12 @@
  * Simple header file for I2C functions that we want to use in main
  * Add any further functions here and define in i2c.c
  */
+//Callback function to read over I2C
 int8_t bhi360_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr);
+//Callback function to write over I2C
 int8_t bhi360_i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr);
+//Callback function to delay for a bit to allow i2c lines to settle
+//Will adjust to work better with FreeRTOS soon.
 void bhi360_delay_us(uint32_t period, void *intf_ptr);
 
 /**
--- FirmwareV0.11/main/main.c
+++ FirmwareV0.11/main/main.c
@@ -3,7 +3,6 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 #include "bhi360.h"
-#include "bhi360_defs.h"
 #include "Bosch_Shuttle3_BHI360.fw.h"
 #include "bhi360_i2c.h"
 #include "stdint.h"
